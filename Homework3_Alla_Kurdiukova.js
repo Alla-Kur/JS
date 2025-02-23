f027993ea0937e977af8ce4e7a9db0945a3d00b8
Alla Kurdiukova <usataalla1617@gmail.com>
2025-02-23 12:52:33 +0200

    Added modified file Homework3_Alla_Kurdiukova.js

--- Homework3_Alla_Kurdiukova.js
+++ Homework3_Alla_Kurdiukova.js
@@ -1,18 +1,33 @@
-function pow() {
-    for (let x = 2; x < 20; x *= 2) {
-      if ( x === 2) {
-       continue;
-      }
-      if (x === 4) {
-        continue;
-      }
-      if (x === 8) {
-        console.log (x);
-      }
-      if (x === 16) {
-        break;
-      }
-    }
-    console.log (`Результат: ${pow}`);
-   }
-  
+//ПЕРЕРОБЛЕНО
+function pow (x, y) {
+    let result = 1;
+      for (let i = 1; i <= y; i++) { 
+      result *= x;
+      };
+      return result;
+    };
+    const numberInDegree = pow (3, 4);
+    console.log(`Результат: ${numberInDegree}`);
+
+
+
+// НЕ КОРРЕКТНЕ ВИКОНАННЯ
+//  function pow() {
+//     for (let x = 2; x < 20; x *= 2) {
+//       if ( x === 2) {
+//        continue;
+//       }
+//       if (x === 4) {
+//         continue;
+//       }
+//       if (x === 8) {
+//         console.log (x);
+//       }
+//       if (x === 16) {
+//         break;
+//       }
+//     }
+//     console.log (`Результат: ${pow}`);
+//  
+
+
