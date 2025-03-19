13aac3e4858acd2ccc822da0acdb7e49ab691e2c
Alla Kurdiukova <usataalla1617@gmail.com>
2025-03-19 18:18:40 +0200

    Added modified file Homework5_Alla_Kurdiukova.js

--- Homework5_Alla_Kurdiukova.js
+++ Homework5_Alla_Kurdiukova.js
@@ -4,66 +4,66 @@ const services = {
     "shaving": "80 uah",
     "washing": "60 uah",
     getCut(){
-        console.log(`servise Cut: ${this.cut}`);
+        console.log(`service Cut: ${this.cut}`);
     },
-    getShaving(){
-        console.log(`servise Shaving: ${this.shaving}`);
-    }, 
-    getWashing(){
-        console.log(`servise Washing: ${this.washing}`);
+    getSum(...services) {
+       let total = 0;
+       for (const service of services) {
+        total += service;      
+     }
+     console.log(`services Sum: ${this.services}`);
     },
+    getMax() {
+        Math.max();
+      console.log(`services Max: ${this.services}`);
+     },
+     getMin() {
+        Math.min();
+      console.log(`services Min: ${this.services}`);
+     }
 };
 services.getCut();
-services.getShaving();
-services.getWashing();
-
+services.getSum();
+services.getMax();
+services.getMin();
 
 services.makeUp = "110 uah";
-services["hairstyle"] = "120 uah"; // Як вивести ці властивості через get не розібралась
+services["hairstyle"] = "120 uah";
 
 // console.log(services);
 
-const {cut, shaving, washing, makeUp, hairstyle} = services;
-// console.log(cut, typeof cut);
-// console.log(shaving, typeof shaving);
-// console.log(washing, typeof washing);
-// console.log(makeUp, typeof makeUp);
-// console.log(hairstyle, typeof hairstyle);
+// const {cut, shaving, washing, makeUp, hairstyle} = services;
+// // console.log(cut, typeof cut);
 
-const cutNum = Number.parseInt(cut);
-const shavingNum = Number.parseInt(shaving);
-const washingNum = Number.parseInt(washing);
-const makeUpNum = Number.parseInt(makeUp);
-const hairstyleNum = Number.parseInt(hairstyle);
+// const cutNum = Number.parseInt(cut);
+// const shavingNum = Number.parseInt(shaving);
+// const washingNum = Number.parseInt(washing);
+// const makeUpNum = Number.parseInt(makeUp);
+// const hairstyleNum = Number.parseInt(hairstyle);
+// // console.log(cutNum, typeof cutNum);
 
-// console.log(cutNum, typeof cutNum);
-// console.log(shavingNum, typeof shavingNum);
-// console.log(washingNum, typeof washingNum);
-// console.log(makeUpNum, typeof makeUpNum);
-// console.log(hairstyleNum, typeof hairstyleNum);
-
-function sum(...numbers) {
-    let total = 0;
-    for (const number of numbers) {
-        total += number;
-    }
-  return  total;
-}
-const price = sum(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
-console.log("The total cost of services: ", price);
+// function sum(...numbers) {
+//     let total = 0;
+//     for (const number of numbers) {
+//         total += number;
+//     }
+//   return  total;
+// }
+// const price = sum(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
+// console.log("The total cost of services: ", price);
 
 
-function min() {
-    return Math.min();
-}
-const minPrice = min(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
-console.log("The minimum cost of the service: ", minPrice); // Результат: Infinity
+// function min() {
+//     return Math.min();
+// }
+// const minPrice = min(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
+// console.log("The minimum cost of the service: ", minPrice); // Результат: Infinity
 
-function max() {
-    return Math.max();
-}
-const maxPrice = max(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
-console.log("The maximum cost of the service: ", maxPrice); // Результат: -Infinity
+// function max() {
+//     return Math.max();
+// }
+// const maxPrice = max(cutNum, shavingNum, washingNum, makeUpNum, hairstyleNum);
+// console.log("The maximum cost of the service: ", maxPrice); // Результат: -Infinity
 
 
 
